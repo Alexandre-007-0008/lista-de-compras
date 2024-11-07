a833a2bc9dd69cbf5cb8a70964ec6867a58b93d3
Alexandre-007-0008 <alexandre.filho@academico.ifpb.edu.br>
2024-11-07 18:01:04 -0300

    src/app/App/ components/actions/login.js

---
+++ src/app/App/ components/actions/login.js
@@ -0,0 +1,22 @@
+'user server'
+
+import axios from "axios"
+import { redirect } from "next/navigation"
+
+import Cookies from 'js-cookie'
+
+export async function login(loginData) {
+	console.log('loginData', loginData)
+
+	const response = await axios.post('/api/v1/auth', {
+		email: loginData.email,
+		password: loginData.password
+	})
+
+	const user = response.data
+	console.log('user', user)
+
+	Cookies.set('user', JSON.stringify(user))
+
+	redirect('/dashboard')
+}
