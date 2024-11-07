011cc3fb2cde96363007398e2ec348854e0af1bd
Alexandre-007-0008 <alexandre.filho@academico.ifpb.edu.br>
2024-11-07 18:19:30 -0300

    src/app/App/ components/producsrcsrc/app/App/ components/products/[slug]/page.js/app/App/ components/api/v1/hello/route.jsts/page.js

---
+++ src/app/App/ components/products/[slug]/page.js
@@ -0,0 +1,20 @@
+export async function generateMetadata({ params, searchParams }, parent) {
+    const { slug } = await params
+    
+    // Resgatar o conteúdo
+  
+    return {
+      title: slug,
+      description: `Página sobre ${slug}`
+    }
+  }
+  
+  export default async function Page({ params }) {
+    const { slug } = await params
+  
+    return (
+      <>
+        Esporte: { slug }
+      </>
+    )
+  }
